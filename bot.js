5df5f83f26e77433c0a442d6faeb6f14d1bf3688
6n6l <43895200+6n6l@users.noreply.github.com>
2019-10-17 01:19:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,15 +18,13 @@ client.on('message', message => {
     const person1 = args[1]
     const person12 = args[2]
     const love = Math.random() * 100;
-    const loveIndex = Math.floor(love / 49);
-    const loveLevel = "♥".repeat(loveIndex) + ":poop:".repeat(1 - loveIndex);
 
     if(message.content.startsWith(prefix + 'حب')) {
         //message.channel.send(` وبين ${args[1]} نسبة الحب بين ${args[2]} هي ${Math.floor(love)} - ${loveLevel}`)
         const embed = new RichEmbed()
         .setColor("#ffb6c1")
         .addField(` ${args[1]} و ${args[2]} نسبة حبكما هي: :cloud:`,
-        `||${Math.floor(love)} %  ${loveLevel}||`)
+        `||${Math.floor(love)}%||`)
         .setTimestamp()
         .setFooter('Made with ♥ by TheBeatz')
         message.channel.send(embed).then(sentMessage => sentMessage.react('♥'))
