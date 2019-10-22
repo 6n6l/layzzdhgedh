7ddba682add7e4373dbee11ed20702c1f7ea250a
6n6l <43895200+6n6l@users.noreply.github.com>
2019-10-22 17:56:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const prefix = '#'
 
 client.on('ready', () => {
 console.log('Online')
-client.user.setGame('Made With ♥ by TheBeatz')
+client.user.setGame('#دعوة')
 })
 
 
@@ -18,18 +18,32 @@ client.on('message', message => {
     const person1 = args[1]
     const person12 = args[2]
     const love = Math.random() * 100;
+    const loveIndex = Math.floor(love / 49);
+    const loveLevel = "♥".repeat(loveIndex) + ":poop:".repeat(1 - loveIndex);
 
     if(message.content.startsWith(prefix + 'حب')) {
         //message.channel.send(` وبين ${args[1]} نسبة الحب بين ${args[2]} هي ${Math.floor(love)} - ${loveLevel}`)
         const embed = new RichEmbed()
         .setColor("#ffb6c1")
-        .addField(` ${args[1]} و ${args[2]} نسبة حبكما هي: :cloud:`,
-        `||${Math.floor(love)}%||`)
+        .addField(` ${args[1]} و ${args[2]} نسبة حبكما هي: 💓`,
+        `||${Math.floor(love)} %  ${loveLevel}||`)
         .setTimestamp()
-        .setFooter('Made with ♥ by TheBeatz')
+        .setFooter('Version 1 | Made with ♥ by TheBeatz')
         message.channel.send(embed).then(sentMessage => sentMessage.react('♥'))
         .catch(console.error);
          }});
+
+
+client.on('message', message => {
+    if(message.content === prefix + 'دعوة') {
+        message.channel.send('**تم إرسال رابط الدعوة لك | ♥**')
+        message.author.send(`**https://discordapp.com/api/oauth2/authorize?client_id=468503366075416586&permissions=268695633&scope=bot
+        
+        Made With ♥ by TheBeatz
+        أي سؤال/ إستفسار / مشاكل قم بالتحدث مع مبرمج البوت
+        <@462752734140039168> - TheBeatz#5170**`)
+    }
+})
 
 
 
